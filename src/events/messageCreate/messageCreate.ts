ff7f923abff46f0177c55199725288558e8ed10a
AALUND13 <tamlund1976@gmail.com>
2024-09-15 10:55:31 -0400

    Add check for `SendMessages` permission before processing messages.

--- src/events/messageCreate/messageCreate.ts
+++ src/events/messageCreate/messageCreate.ts
@@ -1,11 +1,14 @@
-import { Client, Message } from "discord.js";
+import { Client, Message, PermissionFlagsBits, TextChannel } from "discord.js";
 import { GuildAI, openAI } from "../../utils/GuildAI";
 
 const GuildChatAI = new Map<string, GuildAI>();
 
 export function execute(message: Message, client: Client): void {
-    if (message.author.bot) return;
-    if (!message.guild) return;
+    if (
+        message.author.bot || 
+        !message.guild || 
+        !(message.channel as TextChannel).permissionsFor(client.user!)?.has(PermissionFlagsBits.SendMessages)
+    ) return;
 
     if (!GuildChatAI.has(message.guild.id)) {
         GuildChatAI.set(message.guild.id, new GuildAI(message.guild.id, openAI));
